f5ff24164d443af189cf2c2f0c03ca1db139da15
jaelespinosa <jaelespinosa@gmail.com>
2022-07-08 12:01:51 +0200

    add proyectos y tareas models

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import  express  from "express"
 import dotenv from "dotenv"
 import conectarDB from "./config/db.js"
 import usuarioRoutes from './routes/usuarioRoutes.js'
+import proyectoRoutes from './routes/proyectoRoutes.js'
 
 const app = express()
 
@@ -20,6 +21,8 @@ conectarDB()
 //Routing
 
 app.use('/api/usuarios',usuarioRoutes)
+
+app.use('/api/proyectos',proyectoRoutes)
 
 
 const PORT = process.env.PORT || 4000
