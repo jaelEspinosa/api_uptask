cbb0df25d84d560007ca02085bd89fcf3e7a8552
jaelespinosa <jaelespinosa@gmail.com>
2022-07-19 17:56:52 +0200

    add endpoint actualizar estado tarea

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -3,7 +3,14 @@ import Tarea from "../models/Tarea.js"
 import Usuario from "../models/Usuario.js"
 
 const obtenerProyectos = async (req, res) => {
-    const proyectos = await Proyecto.find().where('creador').equals(req.usuario).select('-tareas') 
+    const proyectos = await Proyecto.find({
+      '$or' : [
+        {'colaboradores': {$in: req.usuario}},
+        {'creador': {$in: req.usuario}},
+
+      ]
+    })    
+    .select('-tareas') 
 
     res.json(proyectos)
 }
@@ -28,7 +35,8 @@ const obtenerProyecto = async (req, res) => {
     const error = new Error("No Encontrado")
     return res.status(404).json({msg:error.message})
   }
-  if(proyecto.creador.toString()!== req.usuario._id.toString()){
+  if(proyecto.creador.toString()!== req.usuario._id.toString() && 
+    !proyecto.colaboradores.some(colaborador => colaborador._id.toString()=== req.usuario._id.toString())){
     const error = new Error("Acción no Válida")
     return res.status(401).json({msg:error.message})
   }
